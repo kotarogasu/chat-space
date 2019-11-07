194d26ac4dc16ead3d54f457633cb50d4db6e353
kotarogasu <56858292+kotarogasu@users.noreply.github.com>
2019-11-07 15:07:48 +0900

    Revert ""自動更新機能実装""

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,7 @@
 $(function(){
   function buildMessage(message){
-    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
-    // nilが入らないようにimageが無い場合の定義をしている
-    var html = `<div class="message-view" data-id="${message.id}" >
+    var image = message.image ? `<p class='lower-message__image'><img src="${message.image}"></p>` : '';
+    var html = `<div class="message-view">
                   <div class="message__uper-info">
                     ${message.user_name}
                   </div>
@@ -12,14 +11,12 @@ $(function(){
                   <div class="lower-message">
                     <p class="lower-message__content">
                       ${message.content}
-                    <p class 'lower-message__image'>
                       ${image}
                     </p>
                   </div>
                 </div>`;
     return html ;
   }
-
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -43,37 +40,4 @@ $(function(){
       alert('自動更新に失敗しました');
     });
   })
-
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){
-      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-      last_message_id = $('.message-view:last').data("id"); 
-      $.ajax({
-        //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-        url: 'api/messages',
-        //ルーティングで設定した通りhttpメソッドをgetに指定
-        type: 'get',
-        dataType: 'json',
-        //dataオプションでリクエストに値を含める
-        data: {id: last_message_id}
-      })
-      .done(function(messages) {
-        //追加するHTMLの入れ物を作る
-        console.log(messages)
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        messages.forEach(function (message) {
-        //メッセージが入ったHTMLを取得
-          insertHTML = buildMessage(message); 
-        //メッセージを追加
-          $('.messages').append(insertHTML);
-        })
-        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      })
-      .fail(function() {
-        alert('自動更新に失敗しました');
-      });
-    }
-  };
-  setInterval(reloadMessages, 5000); 
 })
